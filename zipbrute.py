79c53d3d36a7e8a1ba738932a2469938b37ec8eb
Darian Lopez <midwestcoder2019@gmail.com>
2022-11-07 14:20:44 -0600

    Update zipbrute.py

--- zipbrute.py
+++ zipbrute.py
@@ -1,4 +1,5 @@
 import zipfile
+import os
 
 def getZipFileName():
     return input("Enter path to zipfile: ")
@@ -7,13 +8,24 @@ def getPasswordList():
     print("Enter path to a password list  or leave black to use default rockyou.txt")
     return input("Enter path to password list: ")
 
+def assembleFiles():
+    data=[]
+    if not os.path.exists("./rockyou.txt"):
+        for x in os.listdir("./"):
+            if "rockyou_" in x:
+                data.extend(open(x,"r",encoding="utf-8",errors='ignore').readlines())
+                print(len(data))
+    else:
+        data.extend(open('rockyou.txt', "r", encoding="utf-8", errors='ignore').readlines())
+
+    return data
 def main():
     zipName=getZipFileName()
     #init zip file object
     zF = zipfile.ZipFile(zipName)
 
     #get password list or use default
-    passwFile = getPasswordList() or "rockyou.txt"
+    passwFile = getPasswordList() or assembleFiles()
 
     #load passwords
     #opens handle to password file to minimize memory overhead
