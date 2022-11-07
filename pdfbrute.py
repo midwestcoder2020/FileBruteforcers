d1ec45315ab110b0cdbd21a8466f3ff896172b40
Darian Lopez <midwestcoder2019@gmail.com>
2022-11-07 15:10:42 -0600

    Update pdfbrute.py

--- pdfbrute.py
+++ pdfbrute.py
@@ -2,13 +2,14 @@ from PyPDF2 import PdfFileReader
 import os
 
 def getPDFFileName():
-    return input("Enter path to PDF File: ")
+    return input("Enter path to PDF File: ").strip()
 
 def getPasswordList():
     print("Enter path to a password list  or leave black to use default rockyou.txt")
-    return input("Enter path to password list: ")
+    return input("Enter path to password list: ").strip()
 
 def assembleFiles():
+    print("Getting passwords...")
     data=[]
     if not os.path.exists("./rockyou.txt"):
         for x in os.listdir("./"):
@@ -18,16 +19,17 @@ def assembleFiles():
     else:
         data.extend(open('rockyou.txt', "r", encoding="utf-8", errors='ignore').readlines())
 
+    return data
+
 def main():
     pdfFileName=getPDFFileName()
 
     #get password list or use default
     passwFile = getPasswordList() or assembleFiles()
 
-    #load passwords
-    #opens handle to password file to minimize memory overhead
-    with open(passwFile) as passwords:
-        for p in passwords:
+
+    if type(passwFile).__name__ == "list":
+        for p in passwFile:
             print(p.strip())
             file = PdfFileReader(pdfFileName)
             file.decrypt(p.strip().encode())
@@ -38,8 +40,23 @@ def main():
             else:
                 print("Password found:", p)
                 exit(0)
+    else:
+        #load passwords
+        #opens handle to password file to minimize memory overhead
+        with open(passwFile) as passwords:
+            for p in passwords:
+                print(p.strip())
+                file = PdfFileReader(pdfFileName)
+                file.decrypt(p.strip().encode())
+                try:
+                    file.getNumPages()
+                except:
+                    continue
+                else:
+                    print("Password found:", p)
+                    exit(0)
 
-    passwords.close()
+        passwords.close()
 
 if __name__ == '__main__':
     main()
