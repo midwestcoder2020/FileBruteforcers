e0800a686f43594b8d946ff200e93064c0bd33c3
Darian Lopez <midwestcoder2019@gmail.com>
2022-11-07 14:20:25 -0600

    Update pdfbrute.py

--- pdfbrute.py
+++ pdfbrute.py
@@ -1,5 +1,5 @@
 from PyPDF2 import PdfFileReader
-
+import os
 
 def getPDFFileName():
     return input("Enter path to PDF File: ")
@@ -8,12 +8,21 @@ def getPasswordList():
     print("Enter path to a password list  or leave black to use default rockyou.txt")
     return input("Enter path to password list: ")
 
+def assembleFiles():
+    data=[]
+    if not os.path.exists("./rockyou.txt"):
+        for x in os.listdir("./"):
+            if "rockyou_" in x:
+                data.extend(open(x,"r",encoding="utf-8",errors='ignore').readlines())
+                print(len(data))
+    else:
+        data.extend(open('rockyou.txt', "r", encoding="utf-8", errors='ignore').readlines())
 
 def main():
     pdfFileName=getPDFFileName()
 
     #get password list or use default
-    passwFile = getPasswordList() or "rockyou.txt"
+    passwFile = getPasswordList() or assembleFiles()
 
     #load passwords
     #opens handle to password file to minimize memory overhead
